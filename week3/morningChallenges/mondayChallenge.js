35097e41bdd8ff89e78d71a0d273e2686c61b85d
jsms90 <jsms@hotmail.co.uk>
2016-10-31 11:28:21 +0000

    add solution to mondayChallenge

--- week3/morningChallenges/mondayChallenge.js
+++ week3/morningChallenges/mondayChallenge.js
@@ -35,3 +35,39 @@ const parseAddMultiplySubtract = passOutputsAsInputs(
 );
 
 parseAddMultiplySubtract('1'); // 3
+
+// ---------------------------------------------------------------------------
+// Solution from board
+
+var compose = function(...arg) {
+	var rev = arg.reverse();
+	return function(n) {
+		return rev.reduce(function(acc, element) {
+			console.log(acc);
+			return element(acc);
+		}, n);
+	}
+};
+
+var compose2 = function(...arg) {
+	var rev = arg
+	return function(n) {
+		return rev.reduce(function(acc, element) {
+			console.log(acc);
+			return element(acc);
+		}, n);
+	}
+};
+
+function add_one (n) { return n+1; }
+function less_one (n) { return n -1; }
+function multiply_two (n) { return n * 2; }
+function start (n) { return parseInt(n); }
+
+//const parseAddMultiplySubtract = compose(less_one, multiply_two, add_one, start);
+const parseAddMultiplySubtract = compose2(start, add_one, multiply_two, less_one);
+
+var result = less_one(multiply_two(add_one(start("1"))));
+
+console.log("result: ", result);
+parseAddMultiplySubtract('1'); // 3
