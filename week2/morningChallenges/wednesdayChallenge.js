7c3bb9d68aa02ee35447c20039596de2c0ff60c6
jsms90 <jsms@hotmail.co.uk>
2016-10-31 16:13:07 +0000

    wednesday solution

--- week2/morningChallenges/wednesdayChallenge.js
+++ week2/morningChallenges/wednesdayChallenge.js
@@ -23,3 +23,43 @@ console.log(deepEqual(
   'hello',
   'hello'
 )); // -> true
+
+// ---------------------------------------------------------------------------
+// Solution from board
+
+var deepEqual = function(x, y) {
+  if(typeof x !== 'object' && typeof y !== 'object') {
+  	return x === y;
+  } else if (Object.keys(x).length !== Object.keys(y).length) {
+  	return false;
+  } else {
+	return Object.keys(x).every(function(key){
+		return deepEqual(x[key], y[key]);
+	});
+  }
+}
+
+console.log(deepEqual(
+  {},
+  []
+)); // -> false
+
+console.log(deepEqual(
+  [{a:3},{b:4}],
+  [{a:'3'},{b:'4'}]
+)); // -> false
+
+console.log(deepEqual(
+  [{a:3},{b:4},{c:5}],
+  [{a:3},{b:4}]
+)); // -> false
+
+console.log(deepEqual(
+  {a:[2,3],b:[4]},
+  {b:[4],a:[2,3],c:['giraffes','elephants',{b:7}]}
+)); // -> true
+
+console.log(deepEqual(
+  'hello',
+  'hello'
+)); // -> true
