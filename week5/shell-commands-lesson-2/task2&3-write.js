2495a1ecbb303d4ab0a70b796f506fc9bbc2f8b3
jsms90 <jsms@hotmail.co.uk>
2016-11-16 20:42:21 +0000

    rename files & distinguish between append.js and write.js

--- week5/shell-commands-lesson-2/task2&3-write.js
+++ week5/shell-commands-lesson-2/task2&3-write.js
@@ -3,6 +3,9 @@
 const input = process.argv[2];
 const fs = require('fs');
 
+// Task 3
+
+// Append file manually
 let inputRead = fs.createReadStream(input, 'utf8');
 
 if (process.argv[3] === '>>') {
@@ -22,8 +25,15 @@ if (process.argv[3] === '>>') {
     })
   })
 
-  // let writeStream = fs.createWriteStream(output, { 'flags': 'a' });
-  // readStream.pipe(writeStream);
+// Or: append file using 'a' flag
+  /*
+  if (process.argv[3] === '>>') {
+    let writeStream = fs.createWriteStream(output, { 'flags': 'a' });
+    readStream.pipe(writeStream);
+  }
+  */
+
+// Task 2
 } else if (process.argv[3] === '>') {
     let output = process.argv[4];
     let writeStream = fs.createWriteStream(output);
